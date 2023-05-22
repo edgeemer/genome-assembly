7cc27b397d14618d2b51c629dc71a5bc3825ff71
edgeemer <timoshenko.dima2013@gmail.com>
2023-05-22 20:09:56 -0300

    Polishing

--- Scripts/geneious_consensus_reference_length_filter/geneious_consensus_filter.py
+++ Scripts/geneious_consensus_reference_length_filter/geneious_consensus_filter.py
@@ -18,7 +18,7 @@ desc = "\nScript is used for assembly filtration and quality analysis (originall
        "\nIn the OUTPUT DIRECTORY report file is generated." \
        "\n\nDepartment of Biochemistry and Molecular Biology, Dalhousie University\n" \
        "This version is developed and implemented by Dmytro Tymoshenko (RA at the mentioned department), " \
-       "MAY 02/2023, Github <https://github.com/edgeemer>"
+       "MAY 22/2023, Github <https://github.com/edgeemer>"
 
 usage = "\npython <script_name>.py [-h] -i INPUT -o OUTPUT " \
         "[-p PERCENTAGE] [-cs COUNTED_SYMBOLS]\n" \
@@ -33,7 +33,8 @@ parser.add_argument('-i', '--input', type=str, help='Input directory path', requ
 parser.add_argument('-o', '--output', type=str, help='Output directory path', required=True)
 parser.add_argument('-p', '--percentage', type=int, help='Percentage of known nucleotides in sequences (default: 75)',
                     default=75)
-parser.add_argument('-cs', '--counted_symbols', type=str, help='Symbols for subtracting from a sequence (default: ?)',
+parser.add_argument('-cs', '--counted_symbols', type=str, help='Symbols for subtracting from a sequence statistics'
+                                                               ' (default: ?)',
                     default="?")
 
 
@@ -50,7 +51,7 @@ def initialisation():
         percentage: int = args.percentage
         counted_symbols: str = args.counted_symbols
 
-        return input_path, output_path, percentage, counted_symbols
+        return input_path, output_path, percentage, ''.join(set(counted_symbols))  # Exclude repeated symbols
 
     except (ArgumentError, ArgumentTypeError) as e:
         print(f"Check help! Error: {e}")
@@ -67,9 +68,9 @@ def main():
 
     all_symbols = {
         "counted": counted_symbols,
-        "two_nucleotides": "RYMKSW",
-        "three_nucleotides": "HBVD",
-        "all_nucleotides|gap": "N-"
+        "two_nucleotides": "".join(i for i in "RYMKSW" if i not in counted_symbols),
+        "three_nucleotides": "".join(i for i in "HBVD" if i not in counted_symbols),
+        "N|gap": "".join(i for i in "N-" if i not in counted_symbols)
     }
 
     # Create {output_path} if it doesn't exist
@@ -89,10 +90,9 @@ def main():
         for line in lines:
             header = line.strip() if line.startswith(">") else sequences[header].append(line.strip())
 
-        # Create defaultdict to store trimmed sequences (trimmed: input_symbol, both sides to first non input_symbol)
-        # in str type
+        # Create defaultdict to store trimmed sequences (both sides to first non ?) in str type
         str_sequences = defaultdict(str)
-        for header, sequence in sequences.items():
+        for header in sequences.keys():
             str_sequences[header] = ''.join(sequences[header]).strip("?")
 
         # Create/Overwrite the output file in the output directory
@@ -105,9 +105,9 @@ def main():
             """
             Extract reflen (reference length) from header (xxxxx_reflen_1500_xxxx.fasta) and calculate coverage:
                             Trimmed Sequence Length - Number of Input Symbols Inside
-            Similarity = ------------------------------------------------------------  * 100%
+            Coverage = ------------------------------------------------------------  * 100%
                                              Reference Length
-            if reference length is absent, reflen and similarity are equal to zero 
+            if reference length is absent, reflen and coverage are equal to zero 
             """
 
             for header in str_sequences.keys():
@@ -118,66 +118,61 @@ def main():
                         "counted": 0,
                         "two_nucleotides": 0,
                         "three_nucleotides": 0,
-                        "all_nucleotides|gap": 0
+                        "N|gap": 0
                     }
 
-                    # If symbol in COUNTED, we do not calculate it in other groups
+                    # Calculate symbol repetitions by groups
                     for symbol_list_name, symbol_list in all_symbols.items():
                         for symbol in symbol_list:
-                            if symbol in all_symbols["counted"] and symbol_list_name != "counted":
-                                pass
-                            else:
-                                sequence_calculations[symbol_list_name] += str_sequences[header].count(symbol)
+                            sequence_calculations[symbol_list_name] += str_sequences[header].count(symbol)
 
                     # Calculate coverage based on nucleotide types
                     header_arg_list = header.split("_")
                     # If reference length is presented
                     if "reflen" in header_arg_list:
                         reflen = int(header_arg_list[header_arg_list.index("reflen") + 1])
-
-                        not_counted_nucleotides = len(str_sequences[header]) - sequence_calculations["counted"]
+                        seq_length = len(str_sequences[header])
+                        not_counted_nucleotides = seq_length - sequence_calculations["counted"]
                         coverage = int(round(not_counted_nucleotides / reflen * 100))
                     # # If reference length is not presented
                     else:
                         reflen, coverage, = 0, 0
 
-                    # Calculate quality of the sequence on itself
-                    counted = int(round(
-                        sequence_calculations["counted"] / len(str_sequences[header]) * 100))
-                    two_nucleotides = int(round(
-                        sequence_calculations["two_nucleotides"] / len(str_sequences[header]) * 100))
-                    three_nucleotides = int(round(
-                        sequence_calculations["three_nucleotides"] / len(str_sequences[header]) * 100))
-                    all_nucleotides_and_gap = int(round(
-                        sequence_calculations["all_nucleotides|gap"] / len(str_sequences[header]) * 100))
+                    # Calculate parameters of the sequence
+                    counted = int(round((sequence_calculations["counted"] / seq_length) * 100))
+                    two_nucleotides = int(round((sequence_calculations["two_nucleotides"] / seq_length) * 100))
+                    three_nucleotides = int(round((sequence_calculations["three_nucleotides"] / seq_length) * 100))
+                    N_and_gap = int(round((sequence_calculations["N|gap"] / seq_length) * 100))
 
                     # Filter sequences from str_sequences and write them to output file
                     # based on similarity criteria or reference length absence
                     if not reflen or (coverage >= percentage):
                         outfile.write(f"{header}\n{str_sequences[header]}\n")
-                        calculations[header] = {"coverage": coverage,
+                        calculations[header] = {"assembly_length": seq_length,
+                                                "coverage": coverage,
                                                 "counted": counted,
                                                 "two_nucleotides": two_nucleotides,
                                                 "three_nucleotides": three_nucleotides,
-                                                "all_nucleotides|gap": all_nucleotides_and_gap
+                                                "N|gap": N_and_gap
                                                 }
 
         # Open previously created report file
         with open(f'{output_path}/filtration_report.md', 'a+') as report_outfile:
 
             # Templates for lines
-            report_header = '|{:^98}|{:^28}|{:^28}|{:^28}|{:^28}|{:^28}|\n'.format(
-                                                                            'Assembly name',
-                                                                            f'Coverage to reference (>{percentage}%)',
-                                                                            f'Counted NT % ({all_symbols["counted"]})',
-                                                                            f'Other ambiguous % (2 NT)',
-                                                                            f'Other ambiguous % (3 NT)',
-                                                                            f'N/Gap %')
-            empty_line_header = '|:{:^96}:|:{:^26}:|:{:^26}:|:{:^26}:|:{:^26}:|:{:^26}:|\n'.format(
-                '-' * 96, '-' * 26, '-' * 26, '-' * 26, '-' * 26, '-' * 26)
-            plasmid_header_output_line = '|{:^98}|{:^28}|{:^28}|{:^28}|{:^28}|{:^28}|\n'.format(
+            report_header = '|{:^98}' + '|{:^28}' * 6 + '|\n'.format(
+                'Assembly name',
+                f'Assembly length',
+                f'Coverage to reference (>{percentage}%)',
+                f'Counted NT % ({all_symbols["counted"]})',
+                f'Other ambiguous % (2 NT)',
+                f'Other ambiguous % (3 NT)',
+                f'N/Gap %')
+            empty_line_header = '|{:^96}' + '|{:^26}' * 6 + '|\n'.format(
+                '-' * 96, '-' * 26, '-' * 26, '-' * 26, '-' * 26, '-' * 26, '-' * 26)
+            plasmid_header_output_line = '|{:^98}' + '|{:^28}' * 6 + '|\n'.format(
                 f"<b>{input_file.stem}_filtered.fasta</b>",
-                f"{len(calculations.keys())} plasmids", ' ' * 26, ' ' * 26, ' ' * 26, ' ' * 26, )
+                f"{len(calculations.keys())} plasmids", ' ' * 26, ' ' * 26, ' ' * 26, ' ' * 26, ' ' * 26, )
 
             # Create header in case of new file
             if report_outfile.tell() == 0:
@@ -188,16 +183,17 @@ def main():
             report_outfile.write(plasmid_header_output_line)
             # Add similarity result to the relevant plasmid assembly
             for plasmid_assembly_name in calculations.keys():
-                report_outfile.write('|{:^98}|{:^28}|{:^28}|{:^28}|{:^28}|{:^28}|\n'.format(
+                report_outfile.write('|{:^98}' + '|{:^28}' * 6 + '|\n'.format(
                     plasmid_assembly_name.lstrip(">"),
+                    calculations[plasmid_assembly_name]["assembly_length"],
                     calculations[plasmid_assembly_name]["coverage"],
                     calculations[plasmid_assembly_name]["counted"],
                     calculations[plasmid_assembly_name]["two_nucleotides"],
                     calculations[plasmid_assembly_name]["three_nucleotides"],
-                    calculations[plasmid_assembly_name]["all_nucleotides|gap"])
+                    calculations[plasmid_assembly_name]["N|gap"])
                 )
 
-    print("Successful run!")  # For log output
+    print("Successful run!")  # For log
 
 
 if __name__ == '__main__':
